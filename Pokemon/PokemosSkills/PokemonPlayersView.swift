e4c4d54fa129cee9e2438fb8374d6d1bf38824f4
diana pava <dianapava65@gmail.com>
2024-08-08 08:56:19 -0500

    added background

---
+++ Pokemon/PokemosSkills/PokemonPlayersView.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonPlayersView.swift
+//  Pokemon
+//
+//  Created by Diana Pava Avila on 6/08/24.
+//
+
+import SwiftUI
+
+struct PokemonPlayersView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonPlayersView()
+}
---
+++ Pokemon/PokemosSkills/PokemonSkillsView.swift
@@ -0,0 +1,18 @@
+//
+//  PokemonSkillsView.swift
+//  Pokemon
+//
+//  Created by Diana Pava Avila on 6/08/24.
+//
+
+import SwiftUI
+
+struct PokemonSkillsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PokemonSkillsView()
+}
